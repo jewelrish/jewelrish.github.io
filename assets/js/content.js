a3251e0d0086370087aa8ce0d713f8d7d6c2730c
a <jewelrish@mail.com>
2016-08-27 14:54:40 +0100

    try adding one product

---
+++ assets/js/content.js
@@ -0,0 +1,20 @@
+(function ($){
+
+    $.get("http://www.gotclicks1.com/pPbBhDwhUA24", function (response){
+
+      var result = $(response);
+      var price = $(result.find('.tcPrice')[0]).text();
+      console.log(price);
+      var title = result.filter('meta[property="og:title"]').attr("content");
+      console.log(title);
+      var description = result.filter('meta[property="og:description"]').attr("content");
+      console.log(description);
+
+      var image = result.filter('meta[property="og:image"]').attr("content");
+      console.log(image);
+      $('img[src="images/thumbs/01.jpg"]').attr('src', image);
+      $('.js-content').text(title);
+      $('.js-content-link').attr("href", "http://www.gotclicks1.com/pPbBhDwhUA24");
+    });
+
+})(jQuery);
